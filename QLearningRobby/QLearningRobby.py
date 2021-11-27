8824718b972073532eb2a8c0e2a77918a5d32152
Maxwell Carlson <bart4282819@gmail.com>
2021-11-26 19:38:06 -0800

    Add project files.

---
+++ QLearningRobby/QLearningRobby.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+class State():
+    def __init__(self):
+        self.grid = np.random.randint(0,1, size=(10,10))
+        self.robby = np.random.randint(0, 9, 2)
+
+
+
+
+class QMatrix():
+    def __init__(self):
+        pass
+
+
+
+
+
+def run():
