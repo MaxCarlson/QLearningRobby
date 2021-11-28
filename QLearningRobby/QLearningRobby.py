233a02343dd26c0cd9452034f10262bbf6b34aa5
Maxwell Carlson <bart4282819@gmail.com>
2021-11-27 16:17:37 -0800

    Changed q array rep

--- QLearningRobby/QLearningRobby.py
+++ QLearningRobby/QLearningRobby.py
@@ -1,58 +1,5 @@
 import numpy as np
 
-ROBBY_SENSORS_DIR = 4
-ROBBY_INPUTS_DIR = 3
-ROBBY_INPUTS_LOC = 2
-ROBBY_STATES = ROBBY_SENSORS_DIR * ROBBY_INPUTS_DIR * ROBBY_INPUTS_LOC
-
-O = 0
-N = 1
-S = 2
-E = 3
-W = 4
-HAS_CAN = 1
-
-# Notes on states ordering
-# R0: Nothing on any sensor
-# R1: Can on center sensor
-# R2: Can on center sensor, wall on North
-# R3: Can on center sensor, wall on South
-# R4: Can on center sensor, wall on East
-# R5: Can on center sensor, wall on West
-# R6: wall on North
-# R7: wall on South
-# R8: wall on East
-# R9: wall on West
-# R.: Can on Center, wall N & W
-# R.: Can on Center, wall N & E
-# R.: Can on Center, wall S & W
-# R.: Can on Center, wall S & E
-# R.: wall N & W
-# R.: wall N & E
-# R.: wall S & W
-# R.: Can on Center, Can on N
-# R.: Can on Center, Can on S
-# R.: Can on Center, Can on E
-# R.: Can on Center, Can on W
-# R.: Can on Center, Can on N, Can on S
-# R.: Can on Center, Can on N, Can on E
-# R.: Can on Center, Can on N, Can on W
-# R.: Can on Center, Can on S, Can on E
-# R.: Can on Center, Can on S, Can on W
-#.....
-
-
-
-# R.: wall S & E
-# R.: wall S & E
-# R.: wall S & E
-# R.: wall S & E
-
-
-
-
-
-
 class State():
     def __init__(self):
         self.grid = np.random.randint(0,1, size=(10,10))
@@ -64,7 +11,10 @@ class State():
 
 class Robby():
     def __init__(self):
-        self.q = np.zeros((ROBBY_STATES,5))
+        # Dim 1 is no can, or can - currrent square
+        # Dim 2,3,4,5 (N,S,E,W) is empty,can,wall 
+        # Dim 6 are actions, N,S,E,W,Pickup
+        self.q = np.zeros((2, 3, 3, 3, 3, 5))
 
 
 
